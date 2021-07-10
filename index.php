f22bf3e372714dd646afc8d0cf07981b4d249b94
weber-stef <vitatech@gmx.de>
2021-07-10 17:51:52 +0200

    first steps to small template

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );
+/** @var JDocumentHtml $this */
+
+$app  = JFactory::getApplication();
+$user = JFactory::getUser();
+
+// Output as HTML5
+$this->setHtml5(true);
+
+// Add JavaScript Frameworks
+JHtml::_('bootstrap.framework');
+
+// Add template js
+JHtml::_('script', 'template.js', array('version' => 'auto', 'relative' => true));
+
+// Add html5 shiv
+JHtml::_('script', 'jui/html5.js', array('version' => 'auto', 'relative' => true, 'conditional' => 'lt IE 9'));
+
+// Add Stylesheets and 
+JHtml::_('stylesheet', 'template.css', array('version' => 'auto', 'relative' => true));
+
+
+// Check for a custom CSS file
+JHtml::_('stylesheet', 'user.css', array('version' => 'auto', 'relative' => true));
+
+// Check for a custom js file
+JHtml::_('script', 'user.js', array('version' => 'auto', 'relative' => true));
+
+// Load optional RTL Bootstrap CSS
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<jdoc:include type="head" />
+   <link rel="stylesheet" href="media\jui\css\bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+<link rel="styleheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/css/user.css" type="text/css" />
+
+</head>
+<body>
+<jdoc:include type="modules" name="top" /> 
+<jdoc:include type="component" />
+<jdoc:include type="modules" name="footer" />
+</body>
+</html>

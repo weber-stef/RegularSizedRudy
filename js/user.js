5ccd9edbd4f231143cc879a3d61d05b62f14a04d
weber-stef <vitatech@gmx.de>
2021-08-03 00:47:42 +0200

    added fade to links

--- js/user.js
+++ js/user.js
@@ -2,9 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const burgerIcon = document.querySelector(".burger");
     const navigation = document.querySelector(".nav");
     const sayit = "say it";
+    const navLinks = document.querySelectorAll('.nav li')
 
     /* Toggle mobile nav */
     function toggleMenu() {
+
+        navLinks.forEach((link, index) => {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 14}s`;
+
+            console.log(link);
+        })
+
         if (navigation.classList.contains("active")) {
             navigation.classList.remove("active");
             console.log("burger1")
@@ -13,9 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log("burger0")
         }
     }
-    console.log(sayit);
+    // console.log(navLinks);
     /* Event Listener */
     burgerIcon.addEventListener("click", toggleMenu, false);
-
 
 })
